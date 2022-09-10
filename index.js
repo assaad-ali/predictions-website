45459bb3d2b681379fcaba0fb19b513ad0b51fcb
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-10 17:04:20 +0300

    Adding APIs links

---
+++ index.js
@@ -0,0 +1,7 @@
+/********************* APIs **********************/
+
+const gender_api = "https://api.genderize.io/?name="
+const age_api = "https://api.agify.io/?name="
+const nationality_api = "https://api.nationalize.io/?name=mohamad"
+const dog_img_api = "https://dog.ceo/api/breeds/image/random"
+
