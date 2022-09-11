27389ff2878263eb50837a07b89deb4b1087815c
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-12 01:38:12 +0300

    Adding global variables

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const gender_api = "https://api.genderize.io/?name="
 const age_api = "https://api.agify.io/?name="
 const nationality_api = "https://api.nationalize.io/?name="
 const dog_img_api = "https://dog.ceo/api/breeds/image/random"
+
+
+
+/********************* gloval variables **********************/
+
+var name,
+nationality_array
